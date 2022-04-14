86901e31f1f31cde5048d3870282dbd8fafa3895
DonaldDWebster <ddwebster@crimson.ua.edu>
2022-04-14 13:37:13 -0500

    Nonfunctioning Register Files

---
+++ MIS421FinalProjectGit/MIS421FinalProjectGit/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MIS421FinalProjectGit.Areas.Identity.Pages.Account
+{
+    public class ExampleLogInModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
