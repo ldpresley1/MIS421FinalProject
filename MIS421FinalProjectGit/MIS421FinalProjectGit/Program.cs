f25b150a39220081194df798f8ffc022550a3e67
DonaldDWebster <ddwebster@crimson.ua.edu>
2022-04-16 18:16:18 -0500

    Scaffolled Log Out

--- MIS421FinalProjectGit/MIS421FinalProjectGit/Program.cs
+++ MIS421FinalProjectGit/MIS421FinalProjectGit/Program.cs
@@ -4,6 +4,7 @@ using MIS421FinalProjectGit.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
