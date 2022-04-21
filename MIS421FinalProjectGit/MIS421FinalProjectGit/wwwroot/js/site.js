ead447d3a3107fbaf589749fe28e85e1300444ab
IreneBindang <iabesoangue@crimson.ua.edu>
2022-04-21 09:45:56 +0000

    Added  Binance API, modified site Javascript, and updated investment screen

--- MIS421FinalProjectGit/MIS421FinalProjectGit/wwwroot/js/site.js
+++ MIS421FinalProjectGit/MIS421FinalProjectGit/wwwroot/js/site.js
@@ -1,1 +1,11 @@
 // File for your custom JavaScript
+let socket = new WebSocket('wss://stream.binance.com:9443/ws/etheur@trade');
+let stockPriceElement = document.getElementById('stock-ticker');
+let lastPrice = null;
+socket.onmessage = (event) => {
+    let stockObject = JSON.parse(event.data);
+    let price = parseFloat(stockObject.p).toFixed(2);
+    stockPriceElement.innerText = price;
+    stockPriceElement.style.color = !lastPrice || lastPrice === price ? 'black' : price > lastPrice ? 'green' : 'red';
+    lastPrice = price;
+};
