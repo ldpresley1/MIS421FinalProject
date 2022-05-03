f32ec0f6081e5e76732826bb3dc41b00798c624f
DonaldDWebster <ddwebster@crimson.ua.edu>
2022-05-03 16:24:32 -0500

    Allow budgets to be created without having to enter a UserID

--- MIS421FinalProjectGit/MIS421FinalProjectGit/Views/BudgetsController.cs
+++ MIS421FinalProjectGit/MIS421FinalProjectGit/Views/BudgetsController.cs
@@ -60,7 +60,7 @@ namespace MIS421FinalProjectGit.Views
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("BudgetID,BugetItem,Amount,Description,ApplicationUserID")] Budget budget)
+        public async Task<IActionResult> Create([Bind("BudgetID,BugetItem,Amount,Description")] Budget budget)
         {
             if (ModelState.IsValid)
             {
@@ -94,7 +94,7 @@ namespace MIS421FinalProjectGit.Views
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("BudgetID,BugetItem,Amount,Description,ApplicationUserID")] Budget budget)
+        public async Task<IActionResult> Edit(Guid id, [Bind("BudgetID,BugetItem,Amount,Description,")] Budget budget)
         {
             if (id != budget.BudgetID)
             {
